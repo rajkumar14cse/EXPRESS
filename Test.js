2f08021bb0675f40c7164fbd0fec7193c1411943
rajkumar14cse <rajkumargundaboina35@gmail.com>
2024-10-22 18:03:48 +0530

    node

---
+++ Test.js
@@ -0,0 +1,4 @@
+let { add, sub, mul } = require('./Test1')
+console.log("Addition is=" + (add(10, 20)));
+console.log("Subtraction is=" + (sub(20, 10)));
+console.log("multiplication is=" + (mul(20, 10)));
